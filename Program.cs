422f5106b5fb14e5293149656679bc8b696a767f
DilaraGule <80154346+DilaraGule@users.noreply.github.com>
2022-02-16 15:51:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            Console.WriteLine("sayı gir");
+            int sayac = int.Parse(Console.ReadLine());
+            for(int i = 0 ; i < sayac; i++){
+                if(i % 2 == 1)
+                    toplam += i;
+            }
+            Console.WriteLine("Tek sayıların toplamı " + toplam);
+        
+            //break ve continue
+            for(int j = 0; j < 10; j++){
+                if(j == 4)
+                    break;
+                Console.WriteLine(j);
+            }
+
+            for(int j = 0; j < 10; j++){
+                if(j == 4)
+                    continue;
+                Console.WriteLine(j);
+            }
+        }
+
+    }
+}
